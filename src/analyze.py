70313b815fc10e8d4688b792591fb817143ad08e
Izabela Maffra <izabelakarennina@gmail.com>
2014-04-05 20:37:54 -0300

    Kendall Tau corr with unique rank values

--- src/analyze.py
+++ src/analyze.py
@@ -1,8 +1,13 @@
 import argparse
 import os
 import pickle
+import pprint
+import scipy.stats.mstats
+
+from collections import Counter
 
 import networkx as nx
+
 
 _DATA_DIR = os.path.join(os.path.dirname(__file__), '../data')
 
@@ -18,15 +23,12 @@ def read_graph():
 
 def get_cl_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-pagerank', action='store_true', default=False,
+    parser.add_argument('--pagerank', action='store_true', default=False,
                         dest='calc_page_rank',
                         help='Calculate and save PageRank values.')
-    parser.add_argument('-hits', action='store_true', default=False,
+    parser.add_argument('--hits', action='store_true', default=False,
                         dest='calc_hits',
                         help='Calculate and save Hits values.')
-    parser.add_argument('-plot', action='store_true', default=False,
-                        dest='plot',
-                        help='Plot PageRank and Hits distribution.')
     results = parser.parse_args()
     return results
 
@@ -46,7 +48,22 @@ def calc_hits(graph, name):
         pickle.dump(hits, out_file)
         print 'Dumped hits to file %s' % path
 
-    
+def unique_values(my_dict):
+    unique = {}
+    seen = {}
+    for key, value in my_dict.iteritems():
+        if value in seen:
+            other_key = seen[value]
+            if other_key > key:
+                del unique[other_key]
+                unique[key] = value
+                seen[value] = key
+        else:
+            unique[key] = value
+            seen[value] = key
+    return unique
+
+
 def main():
     options = get_cl_options()
     for graph, name in read_graph():
@@ -54,8 +71,40 @@ def main():
             calc_pagerank(graph, name)
         if options.calc_hits:
             calc_hits(graph, name)
-        if options.plot:
-            plot()
+        path = _DATA_DIR + '/' + name
+        with open(path + '_pagerank.pickle') as in_file:
+            pagerank = pickle.load(in_file)
+            pagerank = unique_values(pagerank)
+            print 'Loaded PageRank data from file %s' % path
+            pagerank_rank = [int(x) for x, _ in Counter(pagerank).most_common()]
+        with open(path + '_hits.pickle') as in_file:
+            hubs, auth = pickle.load(in_file)
+            hubs = unique_values(hubs)
+            auth = unique_values(auth)
+            print 'Loaded Hits data from file %s' % path
+            hubs_rank = [int(x) for x, _ in Counter(hubs).most_common()]
+            auth_rank = [int(x) for x, _ in Counter(auth).most_common()]
+        min_len = min(len(pagerank_rank), len(hubs_rank))
+        corr, _ = scipy.stats.kendalltau(pagerank_rank[:min_len],
+                hubs_rank[:min_len])   
+        print ('Kendall tau rank correlation between PageRank and Hubs Rank: %f'
+               % corr)
+        min_len = min(len(pagerank_rank), len(auth_rank))
+        corr, _ = scipy.stats.kendalltau(pagerank_rank[:min_len],
+                auth_rank[:min_len])      
+        print ('Kendall tau rank correlation between PageRank and Authorities Rank: %f'
+               % corr)
+
+        pagerank_ties = Counter(pagerank.values())
+        print 'Unique values: %d out of %d' % (len(pagerank_ties),
+                len(pagerank))
+        auth_ties = Counter(auth.values())
+        print 'Unique values: %d out of %d' % (len(auth_ties),
+                len(auth))
+        hubs_ties = Counter(hubs.values())
+        print 'Unique values: %d out of %d' % (len(hubs_ties),
+                len(hubs))
+        
 
         
 if __name__ == '__main__':
